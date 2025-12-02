7228e69390aae819c6a6bc229ae6795288af884b
Shicheng Rao <shichengrao@users.noreply.github.com>
2025-12-02 00:23:17 -0500

    minor cleanup

--- src/solutions/Day02.java
+++ src/solutions/Day02.java
@@ -13,59 +13,40 @@ public class Day02 extends DayTemplate {
         String[] lines = in.nextLine().split(",");
         for(String line: lines){
             String[] parts = line.split("-");
-            Range r = new Range(Long.parseLong(parts[0]), Long.parseLong(parts[1]));
-            answer+= r.silly2();
-        }
-        return answer+ "";
-    }
-}
-
-class Range{
-    long low;
-    long high;
-    public Range(long l, long h){
-        low = l;
-        high = h;
-    }
-
-    public long silly(){
-        long answer = 0;
-        for(long i = low; i <= high; i++){
-            String test = i + "";
-            int length = test.length();
-            if(length % 2 == 1){
-                continue;
-            }
-            long divisor = (long)Math.pow(10, length/2) + 1;
-            if(i % divisor == 0){
-                answer+=i;
-            }
-        }
-        return answer;
-    }
-
-    public long silly2(){
-        long answer = 0;
-        for(long i = low; i <= high; i++){
-            String test = i + "";
-            int length = test.length();
-            for(int j = 1; j < length; j++){
-                if(length % j == 0){
-                    boolean allSame = true;
-                    String s = test.substring(0,j);
-                    for(int k = 0; k < length / j; k++){
-                        if(!test.substring(k * j, (k + 1) * j).equals(s)){
-                            allSame = false;
+            long low = Long.parseLong(parts[0]);
+            long high = Long.parseLong(parts[1]);
+            for(long i = low; i <= high; i++) {
+                String test = i + "";
+                int length = test.length();
+                if(part1){
+                    if(length % 2 == 1){
+                        continue;
+                    }
+                    long divisor = (long)Math.pow(10, length/2) + 1;
+                    if(i % divisor == 0){
+                        answer+=i;
+                    }
+                }
+                else{
+                    for(int j = 1; j < length; j++){
+                        if(length % j == 0){
+                            boolean allSame = true;
+                            String s = test.substring(0,j);
+                            for(int k = 0; k < length / j; k++){
+                                if(!test.substring(k * j, (k + 1) * j).equals(s)){
+                                    allSame = false;
+                                }
+                            }
+                            if(allSame){
+                                answer+= i;
+                                break;
+                            }
                         }
                     }
-                    if(allSame){
-                        answer+= i;
-                        break;
-                    }
+
                 }
             }
         }
-        return answer;
+        return answer+ "";
     }
-
 }
