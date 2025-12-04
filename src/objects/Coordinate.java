7f0cc06229429288f2071813ddddcbf4325c4985
Shicheng Rao <shichengrao@users.noreply.github.com>
2025-12-04 00:10:34 -0500

    Day 4

--- src/objects/Coordinate.java
+++ src/objects/Coordinate.java
@@ -1,5 +1,8 @@
 package src.objects;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Coordinate {
     public int x;
     public int y;
@@ -19,7 +22,7 @@ public class Coordinate {
         this.z = z;
     }
 
-    public void set(int x, int y){
+    public void set(int x, int y) {
         this.x = x;
         this.y = y;
     }
@@ -32,16 +35,40 @@ public class Coordinate {
         return x == that.x && y == that.y && z == that.z;
     }
 
+    public List<Coordinate> allNeighbors() {
+        List<Coordinate> neighbors = new ArrayList<>();
+        for (int xdelta = -1; xdelta <= 1; xdelta++) {
+            for (int ydelta = -1; ydelta <= 1; ydelta++) {
+                if (xdelta != 0 || ydelta != 0) {
+                    neighbors.add(new Coordinate(x + xdelta, y + ydelta));
+                }
+            }
+        }
+        return neighbors;
+    }
+
+    public List<Coordinate> diagonalNeighbors() {
+        List<Coordinate> neighbors = new ArrayList<>();
+        for (int xdelta = -1; xdelta <= 1; xdelta++) {
+            for (int ydelta = -1; ydelta <= 1; ydelta++) {
+                if (xdelta != 0 && ydelta != 0) {
+                    neighbors.add(new Coordinate(x + xdelta, y + ydelta));
+                }
+            }
+        }
+        return neighbors;
+    }
+
     @Override
     public int hashCode() {
         return 10000 * x + 100 * y + z;
     }
 
-    public String toString(){
+    public String toString() {
         return (x + " " + y);
     }
 
-    public String augmentedToString(){
+    public String augmentedToString() {
         return (x + " " + y + " " + z + " " + weight);
     }
 }
--- src/solutions/Day04.java
+++ src/solutions/Day04.java
@@ -1,13 +1,47 @@
 package src.solutions;
 
 import src.meta.DayTemplate;
+import src.objects.Coordinate;
 
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Day04 extends DayTemplate {
 
     public String solve(boolean part1, Scanner in) {
         long answer = 0;
-        return answer+ "";
+        Set<Coordinate> rolls = new HashSet<>();
+        int row = 0;
+        while (in.hasNextLine()) {
+            String[] line = in.nextLine().split("");
+            for (int col = 0; col < line.length; col++) {
+                if (line[col].equals("@")) {
+                    rolls.add(new Coordinate(row, col));
+                }
+            }
+            row++;
+        }
+        while (true) {
+            Set<Coordinate> forklift = new HashSet<>();
+            for (Coordinate c : rolls) {
+                int neighbors = 0;
+                for (Coordinate neighbor : c.allNeighbors()) {
+                    if (rolls.contains(neighbor)) {
+                        neighbors++;
+                    }
+                }
+                if (neighbors < 4) {
+                    answer++;
+                    forklift.add(c);
+                }
+            }
+            if (forklift.isEmpty() || part1) {
+                break;
+            }
+            rolls.removeAll(forklift);
+        }
+
+        return answer + "";
     }
 }
