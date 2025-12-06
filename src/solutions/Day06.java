6f0825dbb244bbe192f1f8558f6933aee334cf6d
Shicheng Rao <shichengrao@users.noreply.github.com>
2025-12-06 00:45:07 -0500

    cleaner Day 6

--- src/solutions/Day06.java
+++ src/solutions/Day06.java
@@ -10,32 +10,30 @@ public class Day06 extends DayTemplate {
 
     public String solve(boolean part1, Scanner in) {
         long answer = 0;
-        List<Integer> row1 = new ArrayList<>();
-        List<Integer> row2 = new ArrayList<>();
-        List<Integer> row3 = new ArrayList<>();
-        List<Integer> row4 = new ArrayList<>();
-        List<String> row5 = new ArrayList<>();
-        String r1 = in.nextLine();
-        String r2 = in.nextLine();
-        String r3 = in.nextLine();
-        String r4 = in.nextLine();
-        String r5 = in.nextLine();
+        List<String> lines = new ArrayList<>();
+        while(in.hasNextLine()){
+            lines.add(in.nextLine());
+        }
         List<Integer> indices = new ArrayList<>();
-        for(int i = 0; i < r5.length(); i++){
-            if(r5.charAt(i) != ' '){
+        String operatorLine = lines.getLast();
+        lines.removeLast();
+        for(int i = 0; i < operatorLine.length(); i++){
+            if(operatorLine.charAt(i) != ' '){
                 indices.add(i);
             }
         }
+        indices.add(lines.getFirst().length() + 1); //faked operator to be able to get size for the last operator
         for(int i = 0; i < indices.size() - 1; i++){
-            answer += helper2(r1,r2,r3,r4, indices.get(i), indices.get(i + 1) - indices.get(i) - 1, "" + r5.charAt(indices.get(i)));
+            int index = indices.get(i);
+            answer += helper2(lines, index, indices.get(i + 1) - index - 1, operatorLine.charAt(index), part1);
         }
 
         return answer+ "";
     }
 
-    private long helper(List<Integer> ints, String operator){
+    private long helper(List<Integer> ints, char operator){
         long answer = 0;
-        if(operator.equals("+")){
+        if(operator == '+'){
             for(Integer i: ints){
                 answer+= i;
             }
@@ -49,19 +47,21 @@ public class Day06 extends DayTemplate {
         return answer;
     }
 
-    private long helper2(String s1, String s2, String s3, String s4, int index, int length, String operator){
-        String num1 = s1.substring(index, index+length);
-        String num2 = s2.substring(index, index+length);
-        String num3 = s3.substring(index, index+length);
-        String num4 = s4.substring(index, index+length);
+    private long helper2(List<String> strings, int index, int length, char operator, boolean part1){
         List<Integer> ints = new ArrayList<>();
-        for(int i = num1.length() - 1; i >= 0; i--){
-            String tmp = "";
-            tmp+=num1.charAt(i);
-            tmp+=num2.charAt(i);
-            tmp+=num3.charAt(i);
-            tmp+=num4.charAt(i);
-            ints.add(Integer.parseInt(tmp.trim()));
+        if(part1){
+            for(String s: strings){
+                ints.add(Integer.parseInt(s.substring(index, index + length).trim()));
+            }
+        }
+        else{
+            for(int i = length - 1; i >= 0; i--){
+                StringBuilder tmp = new StringBuilder();
+                for(String s: strings){
+                    tmp.append(s.substring(index, index + length).charAt(i));
+                }
+                ints.add(Integer.parseInt(tmp.toString().trim()));
+            }
         }
         return helper(ints, operator);
     }
