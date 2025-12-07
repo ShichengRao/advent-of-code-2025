4b8e25b4ee95d9c2c4ae007b0aface672d80d94f
Shicheng Rao <shichengrao@users.noreply.github.com>
2025-12-07 00:25:51 -0500

    day 7 improvements

--- src/solutions/Day07.java
+++ src/solutions/Day07.java
@@ -12,56 +12,35 @@ public class Day07 extends DayTemplate {
         List<Coordinate> splitters = new ArrayList<>();
         Map<Coordinate, Long> beams = new HashMap<>();
         int row = 0;
-        while(in.hasNextLine()){
+        while (in.hasNextLine()) {
             String[] line = in.nextLine().split("");
-            for(int i = 0; i < line.length; i++){
-                if(line[i].equals("^")){
+            for (int i = 0; i < line.length; i++) {
+                if (line[i].equals("^")) {
                     splitters.add(new Coordinate(row, i));
                 }
-                if(line[i].equals("S")){
+                if (line[i].equals("S")) {
                     beams.put(new Coordinate(row, i), 1L);
                 }
             }
             row++;
         }
-        while(beams.keySet().iterator().next().x < row){
-            System.out.println(beams.keySet().iterator().next().x + " " + beams.size());
-            Map<Coordinate, Long> oldBeams = new HashMap<>();
-            oldBeams.putAll(beams);
+        while (beams.keySet().iterator().next().x < row) {
+            Map<Coordinate, Long> oldBeams = new HashMap<>(beams);
             beams = new HashMap<>();
-            for(Coordinate beam: oldBeams.keySet()){
+            for (Coordinate beam : oldBeams.keySet()) {
                 Coordinate nextBeam = new Coordinate(beam.x + 1, beam.y);
-                if(splitters.contains(nextBeam)){
-                    answer+= oldBeams.get(beam);
-                    Coordinate split1 = new Coordinate(beam.x + 1, beam.y + 1);
-                    Coordinate split2 = new Coordinate(beam.x + 1, beam.y - 1);
-                    if(beams.containsKey(split1)){
-                        beams.put(split1,  oldBeams.get(beam) + beams.get(split1));
-                    }
-                    else{
-                        beams.put(split1,  oldBeams.get(beam));
-                    }
-                    if(beams.containsKey(split2)){
-                        beams.put(split2,  oldBeams.get(beam) + beams.get(split2));
-                    }
-                    else{
-                        beams.put(split2,  oldBeams.get(beam));
-                    }
-                }
-                else{
-                    if(beams.containsKey(nextBeam)){
-                        beams.put(nextBeam,  oldBeams.get(beam) + beams.get(nextBeam));
-                    }
-                    else{
-                        beams.put(nextBeam,  oldBeams.get(beam));
-                    }
+                if (splitters.contains(nextBeam)) {
+                    answer += part1 ? 1 : oldBeams.get(beam);
+                    beams.merge(new Coordinate(beam.x + 1, beam.y + 1), oldBeams.get(beam), Long::sum);
+                    beams.merge(new Coordinate(beam.x + 1, beam.y - 1), oldBeams.get(beam), Long::sum);
+                } else {
+                    beams.merge(nextBeam, oldBeams.get(beam), Long::sum);
                 }
             }
         }
-        long answer2 =0;
-        for(long val: beams.values()){
-            answer2+= val;
+        if (!part1) {
+            answer++; //account for original beam
         }
-        return answer+ " " + answer2;
+        return answer + "";
     }
 }
