73171f61f1e8bdd2ee58e3a864a1d23a0a7e9f01
Shicheng Rao <shichengrao@users.noreply.github.com>
2025-12-11 01:26:00 -0500

    day 11 cleanup

--- src/solutions/Day11.java
+++ src/solutions/Day11.java
@@ -7,61 +7,34 @@ import java.util.*;
 public class Day11 extends DayTemplate {
 
     public String solve(boolean part1, Scanner in) {
-        long answer = 1;
-        List<Node> nodes = new ArrayList<>();
         Map<String, List<String>> map = new HashMap<>();
         while (in.hasNextLine()) {
             String[] line = in.nextLine().split(" ");
-            nodes.add(new Node(line));
-            map.put(nodes.getLast().name, nodes.getLast().childNodes);
+            map.put(line[0].substring(0, line[0].length() - 1), Arrays.asList(line).subList(1, line.length));
         }
-        Map<String, Long> cache = new HashMap<>();
-        if(part1){
-            return helper("you", "out", cache, map) + "";
+        if (part1) {
+            return helper("you", "out", new HashMap<>(), map) + "";
         }
-        answer*= helper("svr", "fft", cache, map);
-        cache = new HashMap<>();
-        answer*= helper("fft", "dac", cache, map);
-        cache = new HashMap<>();
-        answer*= helper("dac", "out", cache, map);
-
+        long answer = 1;
+        answer *= helper("svr", "fft", new HashMap<>(), map);
+        answer *= helper("fft", "dac", new HashMap<>(), map);
+        answer *= helper("dac", "out", new HashMap<>(), map);
         return answer + "";
     }
 
-    private long helper(String curr, String goal, Map<String, Long> cache, Map<String, List<String>> map){
+    private long helper(String curr, String goal, Map<String, Long> cache, Map<String, List<String>> map) {
         long answer = 0;
-        if(curr.equals(goal)){
+        if (curr.equals(goal)) {
             return 1;
-        }
-        else{
-            if(curr.equals("out")){
+        } else {
+            if (map.get(curr) == null) {
                 return 0;
             }
-            for(String child: map.get(curr)){
-                if(cache.containsKey(child)){
-                    answer+=cache.get(child);
-                }
-                else{
-                    answer+=helper(child, goal, cache, map);
-                }
+            for (String child : map.get(curr)) {
+                answer += cache.containsKey(child) ? cache.get(child) : helper(child, goal, cache, map);
             }
         }
         cache.put(curr, answer);
         return answer;
-    }
-}
-
-class Node{
-    String name;
-    List<String> childNodes = new ArrayList<>();
-    List<String> path = new ArrayList<>();
-
-    public Node(String[] line){
-        childNodes = new ArrayList<>();
-        name = line[0].substring(0, line[0].length() - 1);
-        for(int i = 1; i < line.length; i++){
-            childNodes.add(line[i]);
-        }
-        path.add(name);
     }
 }
