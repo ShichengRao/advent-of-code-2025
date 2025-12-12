bfc0da2577e3e386b2b1653d1b5ece91ee322f93
Shicheng Rao <shichengrao@users.noreply.github.com>
2025-12-12 00:21:58 -0500

    Day 12 simplified

--- src/solutions/Day12.java
+++ src/solutions/Day12.java
@@ -2,57 +2,37 @@ package src.solutions;
 
 import src.meta.DayTemplate;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class Day12 extends DayTemplate {
 
     public String solve(boolean part1, Scanner in) {
-        if(!part1){
+        if (!part1) {
             return "Merry Christmas!";
         }
-        List<Integer> shapes = new ArrayList<>();
-        for(int i = 0; i < 6; i++){
+        for (int i = 0; i < 6; i++) {
             in.nextLine();
-            int totalOccupied = 0;
-            totalOccupied += (int)Arrays.stream(in.nextLine().split("")).filter(a -> a.equals("#")).count();
-            totalOccupied += (int)Arrays.stream(in.nextLine().split("")).filter(a -> a.equals("#")).count();
-            totalOccupied += (int)Arrays.stream(in.nextLine().split("")).filter(a -> a.equals("#")).count();
             in.nextLine();
-            shapes.add(totalOccupied);
+            in.nextLine();
+            in.nextLine();
+            in.nextLine();
         }
 
-        int impossible = 0;
         int possible = 0;
-        int inconclusive = 0;
         while (in.hasNextLine()) {
             String[] line = in.nextLine().split(":");
             int length = Integer.parseInt(line[0].split("x")[0]);
             int width = Integer.parseInt(line[0].split("x")[1]);
             String[] nums = line[1].split(" ");
             int naiveUpper = 0;
-            int naiveLower = 0;
-            for(int i = 1; i < nums.length; i++){
+            for (int i = 1; i < nums.length; i++) {
                 int num = Integer.parseInt(nums[i]);
                 naiveUpper += num;
-                naiveLower += num * shapes.get(i - 1);
             }
-            if(naiveUpper <= length/3 * width/3){
+            if (naiveUpper <= length / 3 * width / 3) {
                 possible++;
             }
-            else{
-                if(naiveLower >= length * width){
-                    impossible++;
-                }
-                else{
-                    inconclusive++;
-                }
-            }
-
-
         }
-        return possible+ "";
+        return possible + "";
     }
 }
