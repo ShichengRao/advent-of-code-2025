0a21ae2d8d578ea9ef537b794554956c80900209
Shicheng Rao <shichengrao@users.noreply.github.com>
2026-07-04 23:00:16 -0400

    Clarify Day06 column evaluation

--- src/solutions/Day06.java
+++ src/solutions/Day06.java
@@ -10,59 +10,68 @@ public class Day06 extends DayTemplate {
 
     public String solve(boolean part1, Scanner in) {
         long answer = 0;
+        List<String> lines = readLines(in);
+        String operatorLine = lines.removeLast();
+        List<Integer> operatorColumns = findOperatorColumns(operatorLine, lines.getFirst().length());
+
+        for (int i = 0; i < operatorColumns.size() - 1; i++) {
+            int column = operatorColumns.get(i);
+            int width = operatorColumns.get(i + 1) - column - 1;
+            answer += evaluateColumn(lines, column, width, operatorLine.charAt(column), part1);
+        }
+
+        return answer + "";
+    }
+
+    private List<String> readLines(Scanner in) {
         List<String> lines = new ArrayList<>();
-        while(in.hasNextLine()){
+        while (in.hasNextLine()) {
             lines.add(in.nextLine());
         }
+        return lines;
+    }
+
+    private List<Integer> findOperatorColumns(String operatorLine, int lineLength) {
         List<Integer> indices = new ArrayList<>();
-        String operatorLine = lines.getLast();
-        lines.removeLast();
-        for(int i = 0; i < operatorLine.length(); i++){
-            if(operatorLine.charAt(i) != ' '){
+        for (int i = 0; i < operatorLine.length(); i++) {
+            if (operatorLine.charAt(i) != ' ') {
                 indices.add(i);
             }
         }
-        indices.add(lines.getFirst().length() + 1); //faked operator to be able to get size for the last operator
-        for(int i = 0; i < indices.size() - 1; i++){
-            int index = indices.get(i);
-            answer += helper2(lines, index, indices.get(i + 1) - index - 1, operatorLine.charAt(index), part1);
-        }
-
-        return answer+ "";
+        indices.add(lineLength + 1);
+        return indices;
     }
 
-    private long helper(List<Integer> ints, char operator){
+    private long evaluate(List<Integer> ints, char operator) {
         long answer = 0;
-        if(operator == '+'){
-            for(Integer i: ints){
-                answer+= i;
+        if (operator == '+') {
+            for (Integer i : ints) {
+                answer += i;
             }
-        }
-        else{
+        } else {
             answer = 1;
-            for(Integer i: ints){
-                answer*= i;
+            for (Integer i : ints) {
+                answer *= i;
             }
         }
         return answer;
     }
 
-    private long helper2(List<String> strings, int index, int length, char operator, boolean part1){
+    private long evaluateColumn(List<String> strings, int index, int length, char operator, boolean part1) {
         List<Integer> ints = new ArrayList<>();
-        if(part1){
-            for(String s: strings){
+        if (part1) {
+            for (String s : strings) {
                 ints.add(Integer.parseInt(s.substring(index, index + length).trim()));
             }
-        }
-        else{
-            for(int i = length - 1; i >= 0; i--){
+        } else {
+            for (int i = length - 1; i >= 0; i--) {
                 StringBuilder tmp = new StringBuilder();
-                for(String s: strings){
+                for (String s : strings) {
                     tmp.append(s.substring(index, index + length).charAt(i));
                 }
                 ints.add(Integer.parseInt(tmp.toString().trim()));
             }
         }
-        return helper(ints, operator);
+        return evaluate(ints, operator);
     }
 }
